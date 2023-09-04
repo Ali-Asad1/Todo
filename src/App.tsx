7ebecbd75274f68cdc62ca3d9606ac13d8ad9fee
Ali <aliasad.3128@gmail.com>
2023-09-05 00:17:13 +0330

    Add page routes and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Home from "./pages/Home/Home";
+import Create from "./pages/Create/Create";
+import Update from "./pages/Update/Update";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/:id" element={<Update />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/Update/Update.tsx
@@ -0,0 +1,5 @@
+export default function Update() {
+  return (
+    <div>Update</div>
+  )
+}
