05d0d50c3b9e41de753ef9665b4634ccc516aee4
Ali <aliasad.3128@gmail.com>
2023-09-07 01:47:48 +0330

    Add redux provider

---
+++ src/providers/reduxProvider.tsx
@@ -0,0 +1,7 @@
+import { Provider } from "react-redux";
+import store from "../redux/store";
+
+const ReduxProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return <Provider store={store}>{children};</Provider>;
+};
+export default ReduxProvider;
