98e7d292cd8128d7eb73b3bee778c31f2a120348
Ali <aliasad.3128@gmail.com>
2023-09-06 18:06:15 +0330

    Add Grid layout

--- src/components/shared/Grid.tsx
+++ src/components/shared/Grid.tsx
@@ -1,6 +1,6 @@
 export default function Grid({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container">
+    <div className="desktop:container px-5">
       <div className="grid grid-cols-4 tablet:grid-cols-4 desktop:grid-cols-12 gap-x-5 ">
         {children}
       </div>
