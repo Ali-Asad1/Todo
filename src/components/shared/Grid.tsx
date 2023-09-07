b57c4d8c0e7014b2b3c04e1d90f8aab91caecba2
Ali <aliasad.3128@gmail.com>
2023-09-07 21:01:15 +0330

    Add classNames for grid

--- src/components/shared/Grid.tsx
+++ src/components/shared/Grid.tsx
@@ -1,7 +1,20 @@
-export default function Grid({ children }: { children: React.ReactNode }) {
+import { classNames } from "@/libs/className";
+
+export default function Grid({
+  className,
+  children,
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
   return (
     <div className="desktop:container px-5">
-      <div className="grid grid-cols-4 tablet:grid-cols-4 desktop:grid-cols-12 gap-x-5 ">
+      <div
+        className={classNames(
+          "grid grid-cols-4 tablet:grid-cols-4 desktop:grid-cols-12 gap-x-5",
+          className
+        )}
+      >
         {children}
       </div>
     </div>
