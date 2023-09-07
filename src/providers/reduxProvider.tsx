fe4916e2b67f4b91f8ef293e7d868e00319cd471
Ali <aliasad.3128@gmail.com>
2023-09-07 12:24:48 +0330

    Fix provider bug

--- src/providers/reduxProvider.tsx
+++ src/providers/reduxProvider.tsx
@@ -2,6 +2,6 @@ import { Provider } from "react-redux";
 import store from "../redux/store";
 
 const ReduxProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return <Provider store={store}>{children};</Provider>;
+  return <Provider store={store}>{children}</Provider>;
 };
 export default ReduxProvider;
