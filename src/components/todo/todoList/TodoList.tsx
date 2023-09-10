5f42d66bc5b454ffd9adcce959460dffb5c0d1b5
Ali <aliasad.3128@gmail.com>
2023-09-11 02:16:50 +0330

    Add modal for add new todo

--- src/components/todo/todoList/TodoList.tsx
+++ src/components/todo/todoList/TodoList.tsx
@@ -7,11 +7,15 @@ import Empty from "../Empty";
 import Error from "../Error";
 import Grid from "@/components/shared/Grid";
 import { TodoType } from "@/types/todo.type";
+import { Button } from "@/components/button/Button";
+import { BiPlus } from "react-icons/bi";
+import AddtodoModal from "@/components/modal/todo/AddtodoModal";
 
 const TodoList = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [data, setData] = useState<TodoType[]>([]);
+  const [showModal, setShowModal] = useState(false);
   useEffect(() => {
     fetchTodos();
   }, []);
@@ -32,33 +36,55 @@ const TodoList = () => {
     fetchTodos();
   };
 
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <section>
-      <Grid>
-        <div className="col-span-full desktop:col-span-8 desktop:col-start-3">
-          <div className="w-full min-h-[calc(100vh-240px)] flex flex-col gap-y-5 px-5 py-6 bg-slate-3 rounded-lg border border-slate-6">
-            {loading ? (
-              <>
-                <TodoLoading />
-                <TodoLoading />
-              </>
-            ) : !error ? (
-              data.length ? (
-                <AnimatePresence mode="popLayout">
-                  {data.map((todo: TodoType, index: number) => (
-                    <Todo key={todo.id} {...todo} delay={index} onRemove={removeTodo} />
-                  ))}
-                </AnimatePresence>
-              ) : (
-                <Empty />
-              )
-            ) : (
-              <Error />
-            )}
+    <>
+      <section>
+        <Grid className="mb-5">
+          <div className="desktop:col-start-3">
+            <Button
+              btnType="iconOnly"
+              btnStyle="primary"
+              btnSize="lg"
+              onClick={() => setShowModal(true)}
+            >
+              <BiPlus size={24} />
+            </Button>
           </div>
-        </div>
-      </Grid>
-    </section>
+        </Grid>
+        <Grid>
+          <div className="col-span-full desktop:col-span-8 desktop:col-start-3">
+            <div className="w-full min-h-[calc(100vh-240px)] flex flex-col gap-y-5 px-5 py-6 bg-slate-3 rounded-lg border border-slate-6">
+              {loading ? (
+                <>
+                  {Array(3)
+                    .fill(0)
+                    .map((_, index) => (
+                      <TodoLoading key={index} />
+                    ))}
+                </>
+              ) : !error ? (
+                data.length ? (
+                  <AnimatePresence mode="popLayout">
+                    {data.map((todo: TodoType, index: number) => (
+                      <Todo key={todo.id} {...todo} delay={index} onRemove={removeTodo} />
+                    ))}
+                  </AnimatePresence>
+                ) : (
+                  <Empty />
+                )
+              ) : (
+                <Error />
+              )}
+            </div>
+          </div>
+        </Grid>
+      </section>
+      <AnimatePresence>{showModal && <AddtodoModal onClose={closeModal} />}</AnimatePresence>
+    </>
   );
 };
 export default TodoList;
