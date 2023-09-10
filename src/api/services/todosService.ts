ac2ceade36eb3fb5b6c06f05ed2da1be4eaadca0
Ali <aliasad.3128@gmail.com>
2023-09-10 23:24:58 +0330

    Change fetch responses

--- src/api/services/todosService.ts
+++ src/api/services/todosService.ts
@@ -1,9 +1,23 @@
 import supabase from "@/config/supabase/supabaseClient";
 
 export const getAllTodos = async () => {
-  return await supabase.from("todos").select("*");
+  try {
+    const response = await supabase.from("todos").select("*");
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      return { error: response.error, status: response.status };
+    }
+  } catch (error) {}
 };
 
 export const deleteTodo = async (id: number) => {
-  return await supabase.from("todos").delete().eq("id", id).select();
+  const response = await supabase.from("todos").delete().eq("id", id).select();
+  try {
+    if (response.status === 200) {
+      return { status: 200 };
+    } else {
+      return { error: response.error, status: response.status };
+    }
+  } catch (err) {}
 };
