27106a36540086f7385ee070e8067f0395d83f7d
Ali <aliasad.3128@gmail.com>
2023-09-11 02:15:02 +0330

    Add portal for create portal to dom

---
+++ src/components/shared/Portal.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
+
+const Portal: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const element = document.querySelector("#portal-root");
+  return createPortal(children, element as Element);
+};
+
+export default Portal;
