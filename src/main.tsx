d0458d7235a7c3286711a707db0cda92519547bc
Ali <aliasad.3128@gmail.com>
2023-09-13 13:59:53 +0330

    Add provider to root

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider } from "./contexts/theme.tsx";
 import "./styles/index.css";
 import "./styles/font.css";
 
 ReactDOM.createRoot(document.getElementById("app")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
